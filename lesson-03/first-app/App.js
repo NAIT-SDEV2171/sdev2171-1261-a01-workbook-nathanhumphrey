881f8668135cb5f81895c036896118fce2070b67
Nathan Humphrey <NathanH@nait.ca>
2026-09-16 09:33:15 -0600

    Update first-app with additional components

--- lesson-03/first-app/App.js
+++ lesson-03/first-app/App.js
@@ -4,9 +4,27 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>A new text component</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="dark" />
+      <ScrollView contentContainerStyle={styles.scrollContent}>
+        <View>
+          <Image style={styles.appImage} source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }} />
+          <Text style={styles.title}>Application Name</Text>
+        </View>
+        <View style={styles.card}>
+          <Text style={styles.card.title}>Card Title</Text>
+          <Text style={styles.card.body}>Card body text</Text>
+        </View>
+        <View style={styles.card}>
+          <Text style={styles.card.title}>Card Title</Text>
+          <Text style={styles.card.body}>Card body text</Text>
+          <TextInput
+            defaultValue="A new text input component."
+            multiline
+            style={styles.textInput}
+            placeholder="Type whatever you like here"
+            placeholderTextColor="grey"></TextInput>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -14,7 +32,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f00',
+    backgroundColor: '#FFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
